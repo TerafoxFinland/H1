abfe3f418b4bd770b00bc960687196f8de8ce9d1
kaitsuks <37402060+kaitsuks@users.noreply.github.com>
2020-02-19 15:12:08 +0200

    Siirto
    Valinnan peruminen toisella klikkauksella.

--- Code/MoveSelectedInGridWithMouse.cs
+++ Code/MoveSelectedInGridWithMouse.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
+
 public class MoveSelectedInGridWithMouse : MonoBehaviour 
 {
     public int idNumber; //nappulan tunniste
