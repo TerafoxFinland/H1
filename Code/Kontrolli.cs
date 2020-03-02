b6dce84aa3be041bf5c1d3970442d56381d57568
kaitsuks <37402060+kaitsuks@users.noreply.github.com>
2020-03-02 17:52:11 +0200

    SmoothSwap
    Vaihtaa objektit päittäin

--- Code/Kontrolli.cs
+++ Code/Kontrolli.cs
@@ -11,6 +11,8 @@ public class Kontrolli : MonoBehaviour {
     public bool laattaValittu = false;
     [HideInInspector]
     public bool blokkiValittu = false;
+    [HideInInspector]
+    public bool swapped = false;
     [HideInInspector]    
     public GameObject[] nappulat;
     [HideInInspector]
@@ -22,6 +24,12 @@ public class Kontrolli : MonoBehaviour {
     private Tilemap tilemap;
     Vector3 position;
     Vector3Int gridPos;
+    [HideInInspector]
+    public Vector3 swapPosition;
+    [HideInInspector]
+    public Vector3 swapTarget;
+    [HideInInspector]
+    public Vector3Int swapGridPos;
     //Prefabs 
     public GameObject laatta1;
     public GameObject laatta2;
@@ -32,7 +40,7 @@ public class Kontrolli : MonoBehaviour {
     // Use this for initialization
     void Awake () {
         fRuudukko = new float[10,10];
-        iRuudukko = new int[10, 10, 2];
+        iRuudukko = new int[10, 10, 3]; //koordinaatit, id, tyyppi, merkattu = 1
        
         //Check if instance already exists
         if (instance == null)
@@ -47,10 +55,9 @@ public class Kontrolli : MonoBehaviour {
 
         m_Grid = GameObject.Find("Grid").GetComponent<Grid>();
         tilemap = GameObject.Find("Base").GetComponent<Tilemap>();
-
-        
     }
 
+    //KÄYNNISTETÄÄN NAPPULOIDEN LUONTI
     public void KirjoitaNappulat()
     {
         Debug.Log("KIRJOITTAMISTA PYYDETTY");
@@ -59,6 +66,7 @@ public class Kontrolli : MonoBehaviour {
         //KirjoitaNappulat();
     }
 
+    //LUODAAN NAPPULAT
     IEnumerator CoroutineKirjoitaNappulat()
     {
         //testin ajaksi palataan heti
@@ -89,6 +97,9 @@ public class Kontrolli : MonoBehaviour {
                 newObject.transform.position = position;
                 Vector3Int cellPosition = tilemap.LocalToCell(newObject.transform.localPosition);
                 newObject.transform.localPosition = tilemap.GetCellCenterLocal(cellPosition);
+
+                iRuudukko[j, i, 1] = r; //kirjoitetaan tyyppi ruudukkoon
+
                 //Debug.Log("transform.position = " + transform.position);
                 //s = s + iRuudukko[j, i, 0] + " ";
                 //Debug.Log("Solun tieto 0: ruutu " + j + " " + i + " " + iRuudukko[j, i, 0]); // 
@@ -96,14 +107,13 @@ public class Kontrolli : MonoBehaviour {
             }
         }
        
-
         //yield on a new YieldInstruction that waits for 5 seconds.
         yield return new WaitForSeconds(2);
-
         //After we have waited 2 seconds print the time again.
-        Debug.Log("Finished Coroutine at timestamp : " + Time.time);
+        //Debug.Log("Finished Coroutine at timestamp : " + Time.time);
     }
 
+    //TULOSTETAAN KONSOLILLE
     public void LueNappulat()
     {
         Debug.Log("LUETAAN NAPPULAT... ");
@@ -118,14 +128,17 @@ public class Kontrolli : MonoBehaviour {
         {
             Vector3Int pos = go.GetComponent<MoveSelectedInGridWithMouse>().gridPos;
             int id = go.GetComponent<MoveSelectedInGridWithMouse>().idNumber;
+            int type = go.GetComponent<MoveSelectedInGridWithMouse>().type;
             int ix = pos.x + 5;
             int iy = pos.y + 5;
             Debug.Log("Nappulan " + n + " sijainti: " + pos + " tunnus " + id);
+            Debug.Log("Nappulan " + n + " tyyppe: " +  type);
             Debug.Log("Nappulan " + n + " koordinaatit: x " + ix + " y " + iy);
             n++;
         }
     }
 
+    //TULOSTETAAN KONSOLILLE
     public void LueTaulukko()
     {
         //testin ajaksi palataan heti
@@ -148,8 +161,313 @@ public class Kontrolli : MonoBehaviour {
         print("Solun tieto 0: " + s);
     }
 
-	// Update is called once per frame
-	void Update () {
+    //TÄMÄ METODI EI KÄYTÖSSÄ
+    public void CheckMatches()
+    {
+        HashSet<GameObject> matchedTiles = new HashSet<GameObject>();
+        for (int row = 0; row < 10; row++)
+        {
+            for (int column = 0; column < 10; column++)
+            {
+                GameObject current = GetGameObjectAt(column, row);
+
+                List<GameObject> horizontalMatches = FindColumnMatchForTile(column, row, current);
+                if (horizontalMatches.Count >= 2)
+                {
+                    matchedTiles.UnionWith(horizontalMatches);
+                    matchedTiles.Add(current);
+                }
+
+                List<GameObject> verticalMatches = FindRowMatchForTile(column, row, current);
+                if (verticalMatches.Count >= 2)
+                {
+                    matchedTiles.UnionWith(verticalMatches);
+                    matchedTiles.Add(current);
+                }
+            }
+        }
+
+        foreach (GameObject go in matchedTiles)
+        {
+            //renderer.sprite = null;
+            //go = null;
+        }
+        //Score += matchedTiles.Count;
+        //return matchedTiles.Count > 0;
+        //return matchedTiles.Count > 0;
+    }
+
+    //TÄMÄ METODI EI KÄYTÖSSÄ
+    List<GameObject> FindColumnMatchForTile(int col, int row, GameObject go)
+    {
+        List<GameObject> result = new List<GameObject>();
+        for (int i = col + 1; i < 10; i++)
+        {
+            GameObject nextColumn = GetGameObjectAt(i, row);
+            if (nextColumn.gameObject != go)
+            {
+                break;
+            }
+            result.Add(nextColumn);
+        }
+        return result;
+    }
+
+    //TÄMÄ METODI EI KÄYTÖSSÄ
+    List<GameObject> FindRowMatchForTile(int col, int row, GameObject go)
+    {
+        List<GameObject> result = new List<GameObject>();
+        for (int i = row + 1; i < 10; i++)
+        {
+            GameObject nextRow = GetGameObjectAt(col, i);
+            if (nextRow.gameObject != go)
+            {
+                break;
+            }
+            result.Add(nextRow);
+        }
+        return result;
+    }
+
+    //TÄMÄ METODI EI KÄYTÖSSÄ
+    GameObject GetGameObjectAt(int column, int row)
+    {
+        GameObject tile = null;
+        int n = 0;
+        if (column < 0 || column >= 10
+         || row < 0 || row >= 10)
+            return null;
+        
+            nappulat = GameObject.FindGameObjectsWithTag("Nappula");
+        foreach (GameObject go in nappulat)
+        {
+            Vector3Int pos = go.GetComponent<MoveSelectedInGridWithMouse>().gridPos;
+            int id = go.GetComponent<MoveSelectedInGridWithMouse>().idNumber;
+            int ix = pos.x + 5;
+            int iy = pos.y + 5;
+            Debug.Log("Nappulan " + n + " sijainti: " + pos + " tunnus " + id);
+            Debug.Log("Nappulan " + n + " koordinaatit: x " + ix + " y " + iy);
+            n++;
+            if(iy == row && ix == column) {
+                tile = go;
+            //SpriteRenderer renderer = tile.GetComponent<SpriteRenderer>();
+            //return renderer;
+            }
+        }
+        return tile;
+    }
+
+    // ETSITÄÄN (VÄHINTÄÄN) KOLME SAMAA VAAKA- JA PYSTYSUUNNASSA
+    public void Find3Line()
+    {
+        //testin ajaksi palataan heti
+        //return;
+        //string s = "";
+        int type;
+        int type2;
+        int type3;
+        for (j = 0; j < 10; j++)
+        {
+            for (i = 0; i < 10; i++)
+            {
+                //TSEKKAA VIEREISET PYSTYSUUNNASSA JA MERKITSE
+                if (i < 8)
+                {
+                    type =  iRuudukko[j, i, 1];
+                    type2 = iRuudukko[j, i + 1, 1];
+                    type3 = iRuudukko[j, i + 2, 1];
+                    if (type == type2 && type2 == type3)
+                    {
+                        Debug.Log("Solun tyyppi: ruutu " + j + " " + i + " " + type); // 
+                        //merkkaa solut
+                        iRuudukko[j, i, 2] = 1;
+                        iRuudukko[j, i + 1, 2] = 1;
+                        iRuudukko[j, i + 2, 2] = 1;
+
+                    }
+                }
+                //s = s + iRuudukko[j, i, 0] + " ";
+                Debug.Log("Solu merkitty: " + iRuudukko[j, i, 2]);
+            }
+        }
+
+        for (j = 0; j < 10; j++)
+        {
+            for (i = 0; i < 10; i++)
+            {
+                //TSEKKAA VIEREISET VAAKASUUNNASSA JA MERKITSE
+                if (j < 8)
+                {
+                    type = iRuudukko[j, i, 1];
+                    type2 = iRuudukko[j + 1, i, 1];
+                    type3 = iRuudukko[j + 2, i, 1];
+                    if (type == type2 && type2 == type3)
+                    {
+                        Debug.Log("Solun tyyppi: ruutu " + j + " " + i + " " + type); // 
+                        //merkkaa solut
+                        iRuudukko[j, i, 2] = 1;
+                        iRuudukko[j + 1, i, 2] = 1;
+                        iRuudukko[j + 2, i, 2] = 1;
+
+                    }
+                }
+                //s = s + iRuudukko[j, i, 0] + " ";
+                Debug.Log("Solu merkitty: " + iRuudukko[j, i, 2]);
+            }
+        }
+
+        MarkCells();
+    }
+
+    public void MarkCells()
+    {
+        GameObject nappu;
+        SpriteRenderer ren;
+        Debug.Log("LUETAAN NAPPULAT... ");
+        int n = 1;
+        //if (nappulat == null)
+        {
+
+            nappulat = GameObject.FindGameObjectsWithTag("Nappula");
+        }
+        foreach (GameObject go in nappulat)
+        {
+            nappu = go;
+            Vector3Int pos = go.GetComponent<MoveSelectedInGridWithMouse>().gridPos;
+            //int id = go.GetComponent<MoveSelectedInGridWithMouse>().idNumber;
+            //int type = go.GetComponent<MoveSelectedInGridWithMouse>().type;
+            int ix = pos.x + 5;
+            int iy = pos.y + 5;
+            if(iRuudukko[ix, iy, 2] == 1) //merkitty paikka
+            {
+                ren = nappu.GetComponent<SpriteRenderer>();
+                ren.color = Color.red;
+            }
+            if (iRuudukko[ix, iy, 2] == 2) //merkitty paikka
+            {
+                ren = nappu.GetComponent<SpriteRenderer>();
+                ren.color = Color.green;
+            }
+            //Debug.Log("Nappulan " + n + " sijainti: " + pos + " tunnus " + id);
+            //Debug.Log("Nappulan " + n + " tyyppe: " + type);
+            //Debug.Log("Nappulan " + n + " koordinaatit: x " + ix + " y " + iy);
+            n++;
+
+        }
+
+    }
+
+    public void RemoveCells()
+    {
+        GameObject nappu;
+       
+        Debug.Log("LUETAAN NAPPULAT... ");
+        int n = 1;
+        //if (nappulat == null)
+        {
+
+            nappulat = GameObject.FindGameObjectsWithTag("Nappula");
+        }
+        foreach (GameObject go in nappulat)
+        {
+            nappu = go;
+            Vector3Int pos = go.GetComponent<MoveSelectedInGridWithMouse>().gridPos;
+            //int id = go.GetComponent<MoveSelectedInGridWithMouse>().idNumber;
+            //int type = go.GetComponent<MoveSelectedInGridWithMouse>().type;
+            int ix = pos.x + 5;
+            int iy = pos.y + 5;
+            if (iRuudukko[ix, iy, 2] == 1 || iRuudukko[ix, iy, 2] == 2) //merkitty paikka
+            {
+                Destroy(nappu);
+            }
+            //Debug.Log("Nappulan " + n + " sijainti: " + pos + " tunnus " + id);
+            //Debug.Log("Nappulan " + n + " tyyppe: " + type);
+            //Debug.Log("Nappulan " + n + " koordinaatit: x " + ix + " y " + iy);
+            n++;
+        }
+
+    }
+
+    // ETSITÄÄN POTENTIAALISET KUVIOT
+    public void FindPotentials()
+    {
+        //testin ajaksi palataan heti
+        //return;
+        //string s = "";
+        int type = 0;
+        int type2 = 0;
+        int type3 = 0;
+        for (j = 0; j < 10; j++)
+        {
+            for (i = 0; i < 10; i++)
+            {
+                //TSEKKAA POTENTIAALISET VIEREISET PYSTYSUUNNASSA JA MERKITSE
+                
+                {
+                    type = iRuudukko[j, i, 1];
+                    //type2 = iRuudukko[j, i + 1, 1];
+                    if (i < 7 && j < 7)
+                    {
+                        type3 = iRuudukko[j + 1, i + 1, 1];
+                    
+                    if (type == type3)
+                    {
+                        iRuudukko[j, i, 2] = 2;
+                        //iRuudukko[j - 1, i + 1, 2] = 2;
+                        iRuudukko[j + 1, i + 1, 2] = 2;
+
+                    }
+                    }
+                }
+                //s = s + iRuudukko[j, i, 0] + " ";
+                //Debug.Log("Solu merkitty: " + iRuudukko[j, i, 2]);
+            }
+        }
+
+        for (j = 0; j < 10; j++)
+        {
+            for (i = 0; i < 10; i++)
+            {
+                //TSEKKAA VIEREISET POTENTIAALISET VAAKASUUNNASSA JA MERKITSE
+                //if (j < 7)
+                
+                    
+                    if (i < 7 && j < 7)
+                {
+                    type = iRuudukko[j, i, 1];
+                    type2 = iRuudukko[j + 1, i, 1];
+                    {
+                        type3 = iRuudukko[j + 2, i, 1];
+                    }
+                    if (type == type2 && type2 == type3)
+                    {
+                        Debug.Log("POTENTIAALI MERKITTY !: ruutu " + j + " " + i + " " + type); // 
+                        //merkkaa solut
+                        iRuudukko[j, i, 2] = 2;
+                        iRuudukko[j + 1, i, 2] = 2;
+                        iRuudukko[j + 2, i, 2] = 2;
+
+                    }
+                }
+                //s = s + iRuudukko[j, i, 0] + " ";
+                //Debug.Log("Solu merkitty: " + iRuudukko[j, i, 2]);
+            }
+        }
+
+        MarkCells();
+    }
+
+    public void SwapTiles(Vector3 start, Vector3 target, Vector3Int swapGrid)
+    {
+        swapPosition = start;
+        swapTarget = target;
+        swapGridPos = swapGrid;
+        Debug.Log("SWAP TARGET: " + swapTarget);
+    }
+
+
+    // Update is called once per frame
+    void Update () {
 
         //if (laattaValittu)
         //{
--- Code/MoveSelectedInGridWithMouse.cs
+++ Code/MoveSelectedInGridWithMouse.cs
@@ -12,6 +12,7 @@ public class MoveSelectedInGridWithMouse : MonoBehaviour
     public bool selected = false;
     bool moved = false;
     bool clicked = false;
+    bool swapable = false;
     private GameObject go;
     private Kontrolli kontrolli;
     float speed = 6f;
@@ -26,6 +27,8 @@ public class MoveSelectedInGridWithMouse : MonoBehaviour
     public Vector3Int gridPos;
     [HideInInspector]
     public Vector3Int gridPos0;
+    [HideInInspector]
+    public Vector3Int gridPos2;
     //Vector3 center;
     Tilemap tilemap;
     int ix, iy, fx, fy;
@@ -79,60 +82,110 @@ public class MoveSelectedInGridWithMouse : MonoBehaviour
 
         if (!kontrolli.blokkiValittu && !clicked)
         {
+            targetPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            gridPos = m_Grid.WorldToCell(targetPos);
             clicked = true;
             //Debug.Log("Clicked " + clicked);
             selected = true;
+            kontrolli.swapped = false;
+            swapable = false;
             //Debug.Log("VALINTA TEHTY selected " + selected );
             kontrolli.blokkiValittu = true;
-            //Debug.Log(gameObject + " selected");
+            Debug.Log(gameObject + " selected");
             startPos = transform.position;
             //Debug.Log("START POS " + startPos);
-            gridPos0 = m_Grid.WorldToCell(startPos);
             sr.color = Color.cyan;
+            //kontrolli.SwapTiles(startPos, targetPos);
+            kontrolli.SwapTiles(targetPos, startPos, gridPos);
+
         }
         else
         {
             targetPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             gridPos = m_Grid.WorldToCell(targetPos);
-            if (gridPos0 == gridPos) { Debug.Log("KOHTEET SAMAT");
+            gridPos0 = m_Grid.WorldToCell(startPos);
+            //kontrolli.SwapTiles(startPos, targetPos);
+            //Debug.Log("SWAPATAAN!");
+            //kontrolli.SwapTiles(targetPos, startPos, gridPos);
+            //Invoke("Swap", 0.5f);
+           
+            swapable = true;
+            Debug.Log("KOHDE SWAPATAAN!" + gameObject);
+            //Swap(gameObject);
+
+            if (gridPos0 == gridPos)
+            {
+                Debug.Log("KOHTEET SAMAT");
                 clicked = false;
                 selected = false;
                 kontrolli.blokkiValittu = false;
                 sr.color = Color.white;
-                return; }
-        }
+                return; 
+            }
+            //Invoke("MoveTile", 0.1f);
+        } //MoveTile();
     }
 
     // Update is called once per frame
-    void Update () 
+    void Update()
     {
+        MoveTile();
+        if(!kontrolli.swapped)         Swap(gameObject);
+        
+    }
+
+    public void MoveTile()
+    {
+        //StartCoroutine(MoveCo());
+        MoveIt();
+    }
+
+    public void MoveIt()
+  
+    //IEnumerator MoveCo()
+    { 
         if (m_Grid && Input.GetMouseButtonDown(0) && clicked)
         {
             timeDifference = Time.time - startTime;
             //Debug.Log("Time difference" + timeDifference);
             if (timeDifference > 0.1f)
             {
+               
                 targetPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
                 //Debug.Log("Liikkeelle lähdetty" + targetPos);
                 gridPos = m_Grid.WorldToCell(targetPos);
-                if (gridPos0 == gridPos) return;
+
+
+
+                if (gridPos0 == gridPos) return; // yield return new WaitForSeconds(0.1f); 
                 else
                     moved = true;
                 Debug.Log("gridPos != gridPos0");
             }
+           
+               
         }
-        //if (transform.position != (Vector3) targetPos && selected && moved)
-            if (selected && moved)
-            {
-                transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+        if (selected && moved)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+           
         }
+       
+            
+
+
         if (transform.position == (Vector3) targetPos && selected && moved)
-        { //Debug.Log("Kohde saavutettu" + targetPos);
+        {
+                Debug.Log("1. Kohde saavutettu" + targetPos);
+            //if (!kontrolli.swapped) Swap(gameObject);
+            //if (!kontrolli.swapped && kontrolli.swapPosition == transform.position) Swap();
+            //Swap();
             moved = false;
             selected = false;
             kontrolli.blokkiValittu = false;
             clicked = false;
             sr.color = Color.white;
+            //Swap();
             //Kirjoita taulukkoon
             //KORJATTAVA KOORDINAATIT: LISÄTTÄVÄ ARVO, ETTEI MENE NEGATIIVISELLE
             //TARKISTUS MYÖS, ETTEI MENE YLI RAJAN!
@@ -148,7 +201,74 @@ public class MoveSelectedInGridWithMouse : MonoBehaviour
             //Keskitä ruudukkoon
             Vector3Int cellPosition = tilemap.LocalToCell(transform.localPosition);
             transform.localPosition = tilemap.GetCellCenterLocal(cellPosition);
+
+          //  yield return new WaitForSeconds(2);
         }
+
+
+
+    }
+
+    public void Swap(GameObject go)
+    {
+        StartCoroutine(SwapCo());
+        //SwapIt(go);
+    }
+
+
+
+   IEnumerator SwapCo()
+   //public void SwapIt(GameObject go)
+    {
+       
+        //if (transform.position == (Vector3) targetPos)
+        gridPos2 = m_Grid.WorldToCell(go.transform.position);
+        //if (gridPos2 == kontrolli.swapGridPos && swapable)
+            if (swapable)
+            {
+            sr.color = Color.yellow;
+            //SWAPATTAVA NAPPULA/TIILI
+            //if (!selected && !kontrolli.swapped)
+            if(!kontrolli.swapped)
+            {
+                Debug.Log("LÄHDETÄÄN SWAPPAAMAAN" + targetPos);
+                //targetPos = kontrolli.swapPosition;
+                //targetPos = kontrolli.swapTarget;
+                //gridPos = kontrolli.swapGridPos;
+                transform.position = Vector2.MoveTowards(transform.position, kontrolli.swapTarget, speed * Time.deltaTime);
+                //transform.position = targetPos;
+                //transform.position = kontrolli.swapTarget; 
+
+            }
+            if (transform.position == kontrolli.swapTarget)
+            {
+                Debug.Log("Swap Kohde saavutettu" + kontrolli.swapTarget);
+                kontrolli.swapped = true;
+                swapable = false;
+                //moved = false;
+                //selected = false;
+                //kontrolli.blokkiValittu = false;
+                //clicked = false;
+                sr.color = Color.white;
+                //Kirjoita taulukkoon
+                //KORJATTAVA KOORDINAATIT: LISÄTTÄVÄ ARVO, ETTEI MENE NEGATIIVISELLE
+                //TARKISTUS MYÖS, ETTEI MENE YLI RAJAN!
+                ix = gridPos.x + 5;
+                iy = gridPos.y + 5;
+                if (ix < 0) ix = 0;
+                if (ix > 9) ix = 9;
+                if (iy < 0) iy = 0;
+                if (iy > 9) iy = 9;
+
+                kontrolli.iRuudukko[ix, iy, 0] = idNumber;
+                kontrolli.iRuudukko[ix, iy, 1] = type;
+                //Keskitä ruudukkoon
+                Vector3Int cellPosition = tilemap.LocalToCell(transform.localPosition);
+                transform.localPosition = tilemap.GetCellCenterLocal(cellPosition);
+            }
+            moved = false;
+        }
+      yield return new WaitForSeconds(0.01f);
     }
 
     // Converts a Vector3Int to a [[Vector2Int]].
